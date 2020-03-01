19406ab820873f201f0d8f12e678bfa7434e7ebf
Andrew Yule <andrew.yule@assuredflowsolutionsllc.com>
2020-03-01 15:01:50 -0600

    Formatting: Added in formatting for linearTicks and linearGridLines. Will need to move over source code for these.

--- ggplot/ggplot.m
+++ ggplot/ggplot.m
@@ -12,7 +12,7 @@
 BeginPackage["ggplot`"];
 (* Exported symbols added here with SymbolName::usage *)
 
-ggPlot::usage     = "TBD";
+ggplot::usage     = "TBD";
 geomPoint::usage  = "TBD";
 geomLine::usage   = "TBD";
 
@@ -28,22 +28,58 @@ Begin["`Private`"];
   New UPDATE: It may still be better to use Graphics and then we'll write our own ability to do a scaling function.
 *)
 
-Options[ggPlot] = Prepend[Options[ListPlot], ScalingFunctions -> {Identity, Identity}];
-ggPlot[dataset_, geoms : (geomPoint[__] | geomLine[__] | {(geomPoint[__] | geomLine[__])..}), opts : OptionsPattern[]] := Module[{points, lines, scalingFunctionX, scalingFunctionY, graphicsForEpilog, dataForListPlot, graphic},
+Options[afsPlot] = Join[Options[ListLinePlot], Options[linearTicks], Options[linearGridLines], {afsPlotFrameQ -> True}, {DateTicksFormat -> Automatic}];
+SetOptions[ggplot,
+  ImageSize -> 400, AspectRatio -> 7/10, Frame -> True, Axes -> False,
+  ImageMargins -> Automatic,
+  LabelStyle -> Directive[12, FontFamily -> "Arial"],
+  FrameStyle -> Directive[GrayLevel[0.6], Thickness[0.0008`]],
+  FrameTicksStyle -> Directive[Black, Opacity[1]],
+  FrameTicks -> Automatic, GridLines -> Automatic,  Background -> White,
+  PlotRange -> All
+];
+ggplot[dataset_, geoms : (geomPoint[__] | geomLine[__] | {(geomPoint[__] | geomLine[__])..}), opts : OptionsPattern[]] := Module[{points, lines, scalingFunctionX, scalingFunctionY, graphicsPrimitives, dataForListPlot, graphic},
   points = Cases[geoms, geomPoint[aesthetics__]:> geomPoint[dataset, aesthetics], {0, Infinity}];
   lines = Cases[geoms, geomLine[aesthetics__]:> geomLine[dataset, aesthetics], {0, Infinity}];
 
-  graphicsForEpilog = {Cases[opts, (Epilog -> epi__) :> epi, {0, Infinity}], points, lines} // Flatten;
+  graphicsPrimitives = {Cases[opts, (Epilog -> epi__) :> epi, {0, Infinity}], points, lines} // Flatten;
   (*dataForListPlot = Cases[graphicsForEpilog, {x_?NumericQ, y_?NumericQ}, {0, Infinity}];*)
 
   scalingFunctionX = With[{f = (OptionValue[ScalingFunctions][[1]] //. {s_?StringQ :> ToExpression[s], None -> Identity})}, Function[f[#]]];
   scalingFunctionY = With[{f = (OptionValue[ScalingFunctions][[2]] //. {s_?StringQ :> ToExpression[s], None -> Identity})}, Function[f[#]]];
-  graphicsForEpilog = graphicsForEpilog // ReplaceAll[{x_?NumericQ, y_?NumericQ} :> {scalingFunctionX[x], scalingFunctionY[y]}];
+  graphicsPrimitives = graphicsPrimitives // ReplaceAll[{x_?NumericQ, y_?NumericQ} :> {scalingFunctionX[x], scalingFunctionY[y]}];
 
-  graphic = Graphics[graphicsForEpilog,
-    PlotRange -> All, Frame -> True, GridLines -> Automatic, AspectRatio -> 7/10,
-    (*For use with ListLinePlot and Epilog if we don't want to use Graphics*)
-    (*PlotStyle -> None, Epilog -> graphicsForEpilog,*)
+  graphic = Graphics[graphicsPrimitives,
+    FrameLabel ->
+        If[MatchQ[OptionValue[FrameLabel], {{_?StringQ, _?StringQ}, {_?StringQ, _?StringQ}} | {_?StringQ, _?StringQ} | _?StringQ],
+          (OptionValue[FrameLabel] /. str_?StringQ :> Style[str, Opacity[1], FontColor -> Black]),
+          OptionValue[FrameLabel]
+        ],
+    PlotStyle -> OptionValue[PlotStyle],
+    ImageSize -> OptionValue[ImageSize],
+    AspectRatio -> OptionValue[AspectRatio],
+    Frame -> OptionValue[Frame],
+    Axes -> OptionValue[Axes],
+    LabelStyle -> OptionValue[LabelStyle],
+    FrameStyle -> OptionValue[FrameStyle],
+    FrameTicksStyle -> OptionValue[FrameTicksStyle],
+    FrameTicks -> If[OptionValue[FrameTicks] === Automatic,
+      {
+        {Function[{minY, maxY}, Alex`Plotting`linearTicks[minY, maxY, FilterRules[Join[{opts}, Options[ggplot]], Options[Alex`Plotting`linearTicks]]]], False},
+        {Function[{minX, maxX}, Alex`Plotting`linearTicks[minX, maxX, FilterRules[Join[{opts}, Options[ggplot]], Options[Alex`Plotting`linearTicks]]]], False}
+      },
+      OptionValue[FrameTicks]
+    ],
+    GridLines -> If[OptionValue[GridLines] === Automatic,
+      {
+        Function[{minX, maxX}, Alex`Plotting`linearGridLines[minX, maxX, FilterRules[Join[{opts}, Options[ggplot]], Join[Options[Alex`Plotting`linearTicks], Options[Alex`Plotting`linearGridLines]]]]],
+        Function[{minY, maxY}, Alex`Plotting`linearGridLines[minY, maxY, FilterRules[Join[{opts}, Options[ggplot]], Join[Options[Alex`Plotting`linearTicks], Options[Alex`Plotting`linearGridLines]]]]]
+      },
+      OptionValue[GridLines]
+    ],
+    GridLinesStyle -> Automatic, (* shouldn't need this but do for some reason *)
+    Background -> OptionValue[Background],
+    ImageMargins -> OptionValue[ImageMargins],
     FilterRules[{opts} /. (Epilog -> __) :> Nothing, Options[ListPlot]]
   ];
 
