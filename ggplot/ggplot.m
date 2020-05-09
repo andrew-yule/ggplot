5e88e5e0e065b09b27c84f99ac3273ac49c18e4e
Andrew Yule <andrew.yule@assuredflowsolutionsllc.com>
2020-05-09 12:47:15 -0500

    STILL BROKEN: Working through operator form

--- ggplot/ggplot.m
+++ ggplot/ggplot.m
@@ -15,10 +15,10 @@ ggplot::shapeCount          = "More than 5 discrete shapes are present, aborting
 
 validDatasetQ[dataset_] := MatchQ[dataset, {_?AssociationQ..}];
 
-geomsAndScalesPattern = (geomPoint[___] | geomLine[___] | geomSmooth[___] | geomParityLine[___] | geomHLine[___] | geomVLine[___] | geomCol[___]) ...;
+argPattern = (_Rule | geomPoint) ...;
 
 (* Main ggplot method and entry point *)
-Options[ggplot] = Join[{"data" -> {}}, Options[ListLinePlot], Options[ticks], Options[gridLines](*, Options[Alex`Plotting`linearGridLines],*) (*{DateTicksFormat -> Automatic}*)];
+Options[ggplot] = Join[{"data" -> {}}, Options[ListLinePlot], Options[ticks], Options[gridLines] (*{DateTicksFormat -> Automatic}*)];
 SetOptions[ggplot,
   ImageSize -> 400, AspectRatio -> 7/10, Frame -> True, Axes -> False,
   ImageMargins -> Automatic,
@@ -29,15 +29,15 @@ SetOptions[ggplot,
   PlotRange -> All, PlotRangeClipping -> True
 ];
 Attributes[ggplot] = {HoldAllComplete};
-(*ggplot[dataset_?validDatasetQ, geomsAndScales : geomsAndScalesPattern, options: OptionsPattern[]] := ggplot[geomsAndScales, "data" -> dataset, options];*)
-(*ggplot[geomsAndScales : geomsAndScalesPattern, options: OptionsPattern[]][dataset_?validDatasetQ] := ggplot[geomsAndScales, "data" -> dataset, options];*)
-ggplot[args___] := Catch[Module[{rules, dataset, points, lines, smoothLines, columns, abLines, hLines, vLines, graphicsPrimitives, xScaleType, yScaleType, xScaleFunc, yScaleFunc, xTickFunc, yTickFunc, xGridLineFunc, yGridLineFunc, graphic},
+ggplot[ds_?validDatasetQ, args___] := ggplot["data" -> ds, args];
+ggplot[args___][ds_?validDatasetQ] := ggplot["data" -> ds, args];
+ggplot[args___] := Catch[Module[{options, dataset, points, lines, smoothLines, columns, abLines, hLines, vLines, graphicsPrimitives, xScaleType, yScaleType, xScaleFunc, yScaleFunc, xTickFunc, yTickFunc, xGridLineFunc, yGridLineFunc, graphic},
 
-  rules = Cases[Hold@{args}, _Rule, {2}];
+  options = Cases[Hold@{args}, _Rule, {2}];
 
-  dataset = Lookup[rules, "data", {}];
+  dataset = Lookup[options, "data", {}];
 
-  rules = Join[rules, {"data" -> dataset, "x" -> Lookup[rules, "x", Null], "y" -> Lookup[rules, "y", Null]}];
+  options = Join[options, {"data" -> dataset, "x" -> Lookup[options, "x", Null], "y" -> Lookup[options, "y", Null]}];
 
   (* Switch dates to absolute times *)
   dataset = Replace[dataset, d_?DateObjectQ :> AbsoluteTime[d], Infinity];
@@ -51,19 +51,19 @@ ggplot[args___] := Catch[Module[{rules, dataset, points, lines, smoothLines, col
   yScaleFunc = With[{f = ToExpression[yScaleType /. "Linear" | "Date" -> "Identity"]}, Function[f[#]]];
 
   (* Compile all geom information *)
-  points      = Cases[Hold@{args}, geomPoint[opts___]      :> geomPoint[opts,      FilterRules[rules, Options[geomPoint]], "xScaleFunc" -> xScaleFunc, "yScaleFunc" -> yScaleFunc], {0, Infinity}];
-  lines       = Cases[Hold@{args}, geomLine[opts___]       :> geomLine[opts,       FilterRules[rules, Options[geomLine]], "xScaleFunc" -> xScaleFunc, "yScaleFunc" -> yScaleFunc], {0, Infinity}];
-  smoothLines = Cases[Hold@{args}, geomSmooth[opts___]     :> geomSmooth[opts,     FilterRules[rules, Options[geomSmooth]], "xScaleFunc" -> xScaleFunc, "yScaleFunc" -> yScaleFunc], {0, Infinity}];
-  abLines     = Cases[Hold@{args}, geomParityLine[opts___] :> geomParityLine[opts, FilterRules[rules, Options[geomParityLine]], "xScaleFunc" -> xScaleFunc, "yScaleFunc" -> yScaleFunc], {0, Infinity}];
-  hLines      = Cases[Hold@{args}, geomHLine[opts___]      :> geomHLine[opts,      FilterRules[rules, Options[geomHLine]], "xScaleFunc" -> xScaleFunc, "yScaleFunc" -> yScaleFunc], {0, Infinity}];
-  vLines      = Cases[Hold@{args}, geomVLine[opts___]      :> geomVLine[opts,      FilterRules[rules, Options[geomVLine]], "xScaleFunc" -> xScaleFunc, "yScaleFunc" -> yScaleFunc], {0, Infinity}];
+  points      = Cases[Hold@{args}, geomPoint[opts___]      :> geomPoint[opts,      FilterRules[options, Options[geomPoint]], "xScaleFunc" -> xScaleFunc, "yScaleFunc" -> yScaleFunc], {0, Infinity}];
+  lines       = Cases[Hold@{args}, geomLine[opts___]       :> geomLine[opts,       FilterRules[options, Options[geomLine]], "xScaleFunc" -> xScaleFunc, "yScaleFunc" -> yScaleFunc], {0, Infinity}];
+  smoothLines = Cases[Hold@{args}, geomSmooth[opts___]     :> geomSmooth[opts,     FilterRules[options, Options[geomSmooth]], "xScaleFunc" -> xScaleFunc, "yScaleFunc" -> yScaleFunc], {0, Infinity}];
+  abLines     = Cases[Hold@{args}, geomParityLine[opts___] :> geomParityLine[opts, FilterRules[options, Options[geomParityLine]], "xScaleFunc" -> xScaleFunc, "yScaleFunc" -> yScaleFunc], {0, Infinity}];
+  hLines      = Cases[Hold@{args}, geomHLine[opts___]      :> geomHLine[opts,      FilterRules[options, Options[geomHLine]], "xScaleFunc" -> xScaleFunc, "yScaleFunc" -> yScaleFunc], {0, Infinity}];
+  vLines      = Cases[Hold@{args}, geomVLine[opts___]      :> geomVLine[opts,      FilterRules[options, Options[geomVLine]], "xScaleFunc" -> xScaleFunc, "yScaleFunc" -> yScaleFunc], {0, Infinity}];
   (* columns need a lot more work to sort through *)
   (*columns     = Cases[{geoms}, geomCol[aesthetics__] :> geomCol[dataset, aesthetics, "xScaleFunc" -> xScaleFunc, "yScaleFunc" -> yScaleFunc], {0, Infinity}];*)
 
   graphicsPrimitives = {points, lines, smoothLines, abLines, hLines, vLines} // Flatten;
 
   (* Tick / GridLine functions passed into ggplot FrameTicks -> _ call *)
-  With[{tickOptions = FilterRules[{rules}, Options[ticks]], gridLineOptions = FilterRules[{rules}, Options[gridLines]]},
+  With[{tickOptions = FilterRules[{options}, Options[ticks]], gridLineOptions = FilterRules[{options}, Options[gridLines]]},
     xTickFunc = Function[{min, max}, ticks[xScaleType, min, max, tickOptions]];
     yTickFunc = Function[{min, max}, ticks[yScaleType, min, max, tickOptions]];
 
@@ -71,38 +71,37 @@ ggplot[args___] := Catch[Module[{rules, dataset, points, lines, smoothLines, col
     yGridLineFunc = Function[{min, max}, gridLines[yScaleType, min, max, gridLineOptions]]
   ];
 
-  graphic = Graphics[graphicsPrimitives, Frame -> True, AspectRatio -> 0.7(*,
+  graphic = Graphics[graphicsPrimitives, Frame -> True, AspectRatio -> 0.7,
     FrameLabel ->
-        If[MatchQ[OptionValue[FrameLabel], {{_?StringQ, _?StringQ}, {_?StringQ, _?StringQ}} | {_?StringQ, _?StringQ} | _?StringQ],
-          (OptionValue[FrameLabel] /. str_?StringQ :> Style[str, Opacity[1], FontColor -> Black]),
-          OptionValue[FrameLabel]
+        If[MatchQ[Lookup[options, PlotStyle, OptionValue[ggplot, FrameLabel]], {{_?StringQ, _?StringQ}, {_?StringQ, _?StringQ}} | {_?StringQ, _?StringQ} | _?StringQ],
+          (Lookup[options, PlotStyle, OptionValue[ggplot, FrameLabel]] /. str_?StringQ :> Style[str, Opacity[1], FontColor -> Black]),
+          Lookup[options, PlotStyle, OptionValue[ggplot, FrameLabel]]
         ],
-    PlotStyle -> OptionValue[PlotStyle],
-    ImageSize -> OptionValue[ImageSize],
-    AspectRatio -> OptionValue[AspectRatio],
-    Frame -> OptionValue[Frame],
-    Axes -> OptionValue[Axes],
-    LabelStyle -> OptionValue[LabelStyle],
-    FrameStyle -> OptionValue[FrameStyle],
-    FrameTicksStyle -> OptionValue[FrameTicksStyle],
-    FrameTicks -> If[OptionValue[FrameTicks] === Automatic,
+    PlotStyle         -> Lookup[options, PlotStyle, OptionValue[ggplot, PlotStyle]],
+    ImageSize         -> Lookup[options, ImageSize, OptionValue[ggplot, ImageSize]],
+    AspectRatio       -> Lookup[options, AspectRatio, OptionValue[ggplot, AspectRatio]],
+    Frame             -> Lookup[options, Frame, OptionValue[ggplot, Frame]],
+    Axes              -> Lookup[options, Axes, OptionValue[ggplot, Axes]],
+    LabelStyle        -> Lookup[options, LabelStyle, OptionValue[ggplot, LabelStyle]],
+    FrameStyle        -> Lookup[options, FrameStyle, OptionValue[ggplot, FrameStyle]],
+    FrameTicksStyle   -> Lookup[options, FrameTicksStyle, OptionValue[ggplot, FrameTicksStyle]],
+    FrameTicks        -> If[Lookup[options, FrameTicks, OptionValue[ggplot, FrameTicks]] === Automatic,
       {{yTickFunc, False}, {xTickFunc, False}},
-      OptionValue[FrameTicks]
+      Lookup[options, FrameTicks, OptionValue[ggplot, FrameTicks]]
     ],
-    GridLines -> If[OptionValue[GridLines] === Automatic,
+    GridLines         -> If[Lookup[options, GridLines, OptionValue[ggplot, GridLines]] === Automatic,
       {xGridLineFunc, yGridLineFunc},
-      OptionValue[GridLines]
+      Lookup[options, GridLines, OptionValue[ggplot, GridLines]]
     ],
-    GridLinesStyle -> Automatic, (* shouldn't need this but do for some reason *)
-    Background -> OptionValue[Background],
-    ImageMargins -> OptionValue[ImageMargins],
-    PlotRangeClipping -> OptionValue[PlotRangeClipping],
-    FilterRules[{rules}, Options[ListPlot]]*)
+    GridLinesStyle    -> Automatic, (* shouldn't need this but do for some reason *)
+    Background        -> Lookup[options, Background, OptionValue[ggplot, Background]],
+    ImageMargins      -> Lookup[options, ImageMargins, OptionValue[ggplot, ImageMargins]],
+    PlotRangeClipping -> Lookup[options, PlotRangeClipping, OptionValue[ggplot, PlotRangeClipping]],
+    FilterRules[{options}, Options[ListLinePlot]]
   ];
 
   graphic
 ]];
-(*ggplot[geomsAndScales___, options : OptionsPattern[]][dataset_?validDatasetQ] := ggplot["data" -> dataset, geomsAndScales, options];*)
 
 End[];
 
