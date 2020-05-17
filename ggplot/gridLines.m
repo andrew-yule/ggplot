1d8e53b9aa6a0d347ee260fd83ed52b55315d830
Andrew Yule <andrew.yule@assuredflowsolutionsllc.com>
2020-05-17 14:41:11 -0500

    ticks and gridLines: Slightly tweaked to improve the right styling

--- ggplot/gridLines.m
+++ ggplot/gridLines.m
@@ -9,14 +9,19 @@ Begin["`Private`"];
 
 (* Main gridLine creation function which copies exactly the ticks function, then just reformats the output *)
 (* TODO: the options here are still created and referenced in Alex, need to reconcile that *)
-gridLines[list_?ListQ, opts : OptionsPattern[]] := ReplaceAll[list, {
+Options[formatGridLines] = Join[Options[ticks], Options[gridLines]];
+formatGridLines[list_?ListQ, opts : OptionsPattern[]] := ReplaceAll[list, {
   (*Major ticks*)
   {value_?NumericQ, display : Except[(_?StringQ | _Spacer)], ___} :> {value, OptionValue[majorGridLineStyle2]},
   (*Minor ticks*)
   {value_?NumericQ, display : (_?StringQ | _Spacer), ___} :> {value, OptionValue[minorGridLineStyle2]}
 }];
 
-(* Special method for handling Dates*)
+(* Publicly accessbile tick functions *)
+
+gridLines["Linear" | "Identity", min_?NumericQ, max_?NumericQ, opts : OptionsPattern[]] := formatGridLines[ticks["Linear", min, max, opts]];
+
+(* Special method for handling Dates because we want to go just a little bit further beyond min/max than defaults will do *)
 gridLines["Date", min : (_?NumericQ | _?DateObjectQ), max : (_?NumericQ | _?DateObjectQ), opts: OptionsPattern[]] := Module[{dateTicks, newMin, newMax, dateGridLines},
   dateTicks = Charting`FindDateDivisions[{min, max}, OptionValue[numberOfMajorTicks2]];
   {newMin, newMax} = With[{tickDistance = (#[[2]] - #[[1]]) & @dateTicks[[1;;2, 1]]}, {AbsoluteTime[min] - tickDistance, AbsoluteTime[max] + tickDistance}]; (* Update the min/max to go one more outside of the determined ticks as sometimes date ticks don't include everything the way we would like them to *)
@@ -25,9 +30,11 @@ gridLines["Date", min : (_?NumericQ | _?DateObjectQ), max : (_?NumericQ | _?Date
   dateGridLines
 ];
 
-gridLines[min_?NumericQ, max_?NumericQ, opts : OptionsPattern[]] := gridLines[ticks[min, max, FilterRules[{opts}, Options[ticks]]], FilterRules[{opts}, Options[gridLines]]];
+gridLines["Log", min_?NumericQ, max_?NumericQ, opts : OptionsPattern[]] := formatGridLines[ticks["Log", min, max, opts]];
+gridLines["Log10", min_?NumericQ, max_?NumericQ, opts : OptionsPattern[]] := formatGridLines[ticks["Log10", min, max, opts]];
+gridLines["Log2", min_?NumericQ, max_?NumericQ, opts : OptionsPattern[]] := formatGridLines[ticks["Log2", min, max, opts]];
 
-gridLines[func_?StringQ, min_?NumericQ, max_?NumericQ, opts : OptionsPattern[]] := gridLines[ticks[func, min, max, FilterRules[{opts}, Options[ticks]]], FilterRules[{opts}, Options[gridLines]]];
+gridLines["Discrete", lbls_?ListQ, opts : OptionsPattern[]] := formatGridLines[ticks["Discrete", lbls, opts]];
 
 End[];
 
--- ggplot/ticks.m
+++ ggplot/ticks.m
@@ -25,27 +25,32 @@ formatTicks[list_?ListQ, opts : OptionsPattern[]] := ReplaceAll[list, {
 
 ticks["Linear" | "Identity", min_?NumericQ, max_?NumericQ, opts : OptionsPattern[]] := formatTicks[Charting`ScaledTicks["Identity"][min, max, {OptionValue[numberOfMajorTicks2], OptionValue[numberOfMinorTicksPerMajorTick2]}], opts];
 
-(* Special method for handling Dates*)
+(* Special method for handling Dates *)
 ticks["Date", min : (_?NumericQ | _?DateObjectQ), max : (_?NumericQ | _?DateObjectQ), opts: OptionsPattern[]] := Module[{dateTicks, newMin, newMax},
   dateTicks = Charting`FindDateDivisions[{min, max}, OptionValue[numberOfMajorTicks2]];
   {newMin, newMax} = With[{tickDistance = (#[[2]] - #[[1]]) & @dateTicks[[1;;2, 1]]}, {AbsoluteTime[min] - tickDistance, AbsoluteTime[max] + tickDistance}]; (* Update the min/max to go one more outside of the determined ticks as sometimes date ticks don't include everything the way we would like them to *)
   dateTicks = Charting`FindDateDivisions[{newMin, newMax}, OptionValue[numberOfMajorTicks2]];
-  dateTicks = dateTicks /. {value_?NumericQ, dateString_,___} :> {value, If[OptionValue[DateTicksFormat] === Automatic, dateString, DateString[value, OptionValue[DateTicksFormat]]], OptionValue[majorTickLength2], OptionValue[majorTickStyle2]};
+  (* Note: using NumberForm as a little trick on the label as String will mess up pattern matching when creating gridlines *)
+  dateTicks = dateTicks /. {value_?NumericQ, dateString_ ,___} :> {value, If[OptionValue[DateTicksFormat] === Automatic, NumberForm[dateString], NumberForm[DateString[value, OptionValue[DateTicksFormat]]]], OptionValue[majorTickLength2], OptionValue[majorTickStyle2]};
   dateTicks
 ];
 
 (* Log, Log10, Log2, or Reverse *)
-ticks["Log", min_?NumericQ, max_?NumericQ, opts : OptionsPattern[]] := formatTicks[Charting`ScaledTicks["Log"][min, max, {OptionValue[numberOfMajorTicks2], OptionValue[numberOfMinorTicksPerMajorTick2]}], opts];
+ticks["Log", min_?NumericQ, max_?NumericQ, opts : OptionsPattern[]]   := formatTicks[Charting`ScaledTicks["Log"][min, max, {OptionValue[numberOfMajorTicks2], OptionValue[numberOfMinorTicksPerMajorTick2]}], opts];
 ticks["Log10", min_?NumericQ, max_?NumericQ, opts : OptionsPattern[]] := formatTicks[Charting`ScaledTicks["Log10"][min, max, {OptionValue[numberOfMajorTicks2], OptionValue[numberOfMinorTicksPerMajorTick2]}], opts];
-ticks["Log2", min_?NumericQ, max_?NumericQ, opts : OptionsPattern[]] := formatTicks[Charting`ScaledTicks["Log2"][min, max, {OptionValue[numberOfMajorTicks2], OptionValue[numberOfMinorTicksPerMajorTick2]}], opts];
+ticks["Log2", min_?NumericQ, max_?NumericQ, opts : OptionsPattern[]]  := formatTicks[Charting`ScaledTicks["Log2"][min, max, {OptionValue[numberOfMajorTicks2], OptionValue[numberOfMinorTicksPerMajorTick2]}], opts];
 
 (* Special method for handling discrete ticks (i.e. things like strings) *)
 ticks["Discrete", lbls_?ListQ, opts : OptionsPattern[]] := Module[{numberOfLabels, majorTicks, minorTicks, allTicks},
   numberOfLabels = Length[lbls];
-  {majorTicks, minorTicks} = FindDivisions[{1, numberOfLabels}, {numberOfLabels, OptionValue[numberOfMinorTicksPerMajorTick2]}];
-  majorTicks = MapIndexed[Function[{value, index}, {value, lbls[[First@index]], OptionValue[majorTickLength2], OptionValue[majorTickStyle2]}], majorTicks];
-  minorTicks = Map[Function[{value}, {value, "", OptionValue[minorTickLength2], OptionValue[minorTickStyle2]}], DeleteDuplicates[Flatten[minorTicks]]];
-  allTicks = Join[majorTicks, minorTicks];
+  {majorTicks, minorTicks} = If[OptionValue[numberOfMinorTicksPerMajorTick2] === 1,
+    Join[FindDivisions[{1, numberOfLabels}, {numberOfLabels}], {{}}],
+    FindDivisions[{1, numberOfLabels}, {numberOfLabels, OptionValue[numberOfMinorTicksPerMajorTick2]}]
+  ];
+  (* Note: using NumberForm as a little trick on the label as String will mess up pattern matching when creating gridlines *)
+  majorTicks = MapIndexed[Function[{value, index}, {value, NumberForm[lbls[[First@index]]], OptionValue[majorTickLength2], OptionValue[majorTickStyle2]}], majorTicks];
+  minorTicks = Map[Function[{value}, {value, Spacer[{0, 0}], OptionValue[minorTickLength2], OptionValue[minorTickStyle2]}], DeleteDuplicates[Flatten[minorTicks]]];
+  allTicks = Join[majorTicks, Cases[minorTicks, {x_?NumericQ, ___} /; !MemberQ[majorTicks[[All, 1]], x]]];
   allTicks
 ];
 
